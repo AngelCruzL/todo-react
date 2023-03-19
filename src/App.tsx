e7426766cedf0438b8704ace9fa5a727d02bfa8e
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-03-19 08:46:39 -0600

    feat: set up custom material UI theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { FC, ReactElement } from 'react';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+import { customTheme } from './theme/customTheme';
 
 const App: FC = (): ReactElement => {
-  return <h1>Hello World</h1>;
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+      
+      <div>Hello World</div>
+    </ThemeProvider>
+  );
 };
 
 export default App;

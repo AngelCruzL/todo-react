101c9e37559ed51d448691c04813346318309b51
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-03-20 09:14:12 -0600

    chore: set up vitest to execute tests

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+// https://stackoverflow.com/questions/72146352/vitest-defineconfig-test-does-not-exist-in-type-userconfigexport/73106019#73106019
+
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+});

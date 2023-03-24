0a9a3d302490e9280ad13f02600f96235b03ff53
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-03-24 11:45:27 -0600

    feat: add taskArea.tsx layout

--- src/components/TaskArea/taskArea.tsx
+++ src/components/TaskArea/taskArea.tsx
@@ -1,11 +1,49 @@
 import { FC, ReactElement } from 'react';
-
-import { Grid } from '@mui/material';
+import { Box, Grid } from '@mui/material';
+import { format } from 'date-fns';
 
 const TaskArea: FC = (): ReactElement => {
   return (
     <Grid item md={8} px={4}>
-      <h2>Contend Area</h2>
+      <Box mb={8} px={4}>
+        <h2>
+          Status of your Tasks as on{' '}
+          {format(new Date(), 'PPPP')}
+        </h2>
+      </Box>
+
+      <Grid
+        container
+        display="flex"
+        justifyContent="center"
+      >
+        <Grid
+          item
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-around"
+          alignItems="center"
+          md={10}
+          xs={12}
+          mb={8}
+        >
+          <Box>task</Box>
+          <Box>counter</Box>
+          <Box>counter</Box>
+        </Grid>
+
+        <Grid
+          item
+          display="flex"
+          flexDirection="column"
+          md={8}
+          xs={10}
+        >
+          <Box>task</Box>
+          <Box>counter</Box>
+          <Box>counter</Box>
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
